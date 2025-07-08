7d316d60e46b43d836d3c588f1b469eb7606884d
mannpadhiar <mannpadhiar22@gmail.com>
2025-07-08 14:09:29 +0530

    added lab-6

---
+++ lab-6/quick_sort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include<time.h>
+
+int partition(int arr[], int low, int high) {
+    int pivot = arr[low];
+    int i = low + 1;
+    int j = high;
+
+    while (i <= j) {
+        while (i <= high && arr[i] <= pivot) {
+            i++;
+        }
+        while (j >= low && arr[j] > pivot) {
+            j--;
+        }
+        if (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[low];
+    arr[low] = arr[j];
+    arr[j] = temp;
+
+    return j;  
+}
+
+void quickSort(int arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void main() {
+    
+
+    int arr[] = {7, 4, 5, 9, 3, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
